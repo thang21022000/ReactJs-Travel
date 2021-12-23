e1b1f850c5b880764e506de4ae429fb6f47b740b
thang21022000 <thng21022000@gmail.com>
2021-12-23 20:48:52 +0700

    add server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 5000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
