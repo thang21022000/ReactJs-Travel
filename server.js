8ab29097f120ab5c6f6426989bdfaf2e87709b4c
thang21022000 <thng21022000@gmail.com>
2021-12-23 21:43:05 +0700

    fix 5000

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { create, router as _router, defaults } from './json-server';
 const server = create();
 const router = _router('db.json');
 const middlewares = defaults();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.use(middlewares);
 server.use(router);
