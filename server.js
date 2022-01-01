3b328bd21e92aacf9297fcf292775a3a802b0ccc
thang21022000 <72341448+thang21022000@users.noreply.github.com>
2022-01-01 14:08:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
 const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json');
 const port = process.env.PORT || 5000;
 
-server.use(middlewares);
-server.use(router);
+app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
 
-server.listen(port, () => {
-    console.log(`Test Custom Data API listening on port ${port}!`)
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+server.listen(port);
