60836d7835a3efdc029f2c8a28a90692fa57923d
thang21022000 <72341448+thang21022000@users.noreply.github.com>
2022-01-01 14:22:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const jsonServer = require('json-server');
-const app = jsonServer.create();
-const path = require('path');
 const express = require('express');
-const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
-const port = process.env.PORT || 5000;
-
-app.use('/db', middlewares, router);
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-server.listen(port);
+app.listen(process.env.PORT || 8080);
