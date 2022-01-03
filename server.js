e734338b868becaa0b6fe5f192a2460b23fa573c
thang21022000 <thng21022000@gmail.com>
2022-01-03 16:44:50 +0700

    change server

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 5000;
 
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
-const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
+server.use('/db', middlewares, router);
+server.use(express.static(path.join(__dirname, 'build')));
+
+server.get('/ping', function (req, res) {
  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+server.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+server.listen(port);
