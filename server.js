1188179584f34ef776be83201089be4ecaca0f21
thang21022000 <thng21022000@gmail.com>
2022-01-03 16:33:33 +0700

    change server

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const jsonServer = require('json-server');
-const app = jsonServer.create();
-const path = require('path');
-const express = require('express');
-const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 5000;
 
-app.use('/db', middlewares, router);
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
-  return res.send('pong');
- });
- 
- app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+ return res.send('pong');
+});
 
-server.listen(port);
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
